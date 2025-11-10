2eb81be0ae65ca6e2ee096cb22a96ad5d3e512dd
Deepakg-code <deepakadhi0808@gmail.com>
2025-11-11 03:11:30 +0530

    Initial commit

---
+++ src/main/java/com/rec/fs/RenewableEnergyForecastingApplication.java
@@ -0,0 +1,13 @@
+package com.rec.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewableEnergyForecastingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenewableEnergyForecastingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rec/fs/RenewableEnergyForecastingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rec.fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenewableEnergyForecastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
